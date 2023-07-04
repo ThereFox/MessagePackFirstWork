2a3910d0a9d8a41476f5bb96ae2e78ed0e90f7f6
ThereFox <alexandrgrizunow@gmail.com>
2023-07-04 22:47:58 +0400

    init

---
+++ MessagePackTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using MessagePack;
+using System.Text.Json;
+
+var person = new Person(123, "test", 123);
+
+var data = MessagePackSerializer.Serialize<Person>(person);
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.Write($"{data[i]:X} ");
+}
+
+var initData = MessagePackSerializer.Deserialize<Person>(data);
+
+var json = JsonSerializer.Serialize<Person>(person);
+
+Console.WriteLine(initData.ToString());
+
+
+[MessagePackObject(false)]
+public class Person
+{
+    [Key(1)]
+    public int Id { get; set; }
+    [Key(2)]
+    public string Name { get; set; }
+    [Key(3)]
+    public int Age { get; set; }
+
+
+    public Person(int id, string name, int age)
+    {
+        Id = id;
+        Name= name;
+        Age= age;
+    }
+
+}
